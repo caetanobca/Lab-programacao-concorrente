7e27f8185da276471f92121864b841a8eb66d775
Caetano Albuquerque <caetano.albuquerque@ccc.ufcg.edu.br>
2023-02-03 14:20:14 -0300

    Removendo imports desnecessarios

--- Pc-Lab1/src/main/java/main/lab1/App.java
+++ Pc-Lab1/src/main/java/main/lab1/App.java
@@ -3,9 +3,6 @@ package main.lab1;
 import main.lab1.DTOs.Actor;
 import main.lab1.DTOs.Movie;
 import main.lab1.DTOs.MainActors;
-import main.lab1.services.ActorWorker;
-import main.lab1.services.MoviesWorker;
-import main.lab1.services.RatingWorker;
 import main.lab1.utils.Utils;
 
 import java.io.IOException;
@@ -18,15 +15,14 @@ import java.util.concurrent.Semaphore;
 public class App {
     public static void main(String[] args) throws InterruptedException {
         long startTime = System.nanoTime();
-
         String path = "../data/actors.txt";
         int size = 1000;
 
-        try {
-            Utils.csvWriter(actorsRatting, "result.csv");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        // try {
+        //     Utils.csvWriter(actorsRatting, "result.csv");
+        // } catch (IOException e) {
+        //     throw new RuntimeException(e);
+        // }
         System.out.println("\n--------------------------------------------------" +
                 "\nTotal execution time in millis: " + ((System.nanoTime() - startTime)/1000000));
     }
